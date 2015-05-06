b8f8afc34724a641f072de0875136d444027fd6e
Hackbright Student <info@hackbrightacademy.com>
2015-05-05 17:52:11 -0700

    Initialized step 2: created base, homepage & user_list.html. Next: add login form and route that shows it.

--- model.py
+++ model.py
@@ -39,9 +39,22 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=True)
-    user_id = db.Column(db.Integer, nullable=True)
-    score = db.Column(db.Integer, nullable=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    score = db.Column(db.Integer)
+
+     # Define relationship to user
+    user = db.relationship('User', backref=db.backref('ratings', order_by=rating_id))
+
+    # Define relationship to movie
+    movie = db.relationship('Movie', backref=db.backref('ratings', order_by=rating_id))
+ 
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Rating rating_id=%s movie_id=%s user_id=%s score=%s>" % (
+            self.rating_id, self.movie_id, self.user_id, self.score)
 
 
 
